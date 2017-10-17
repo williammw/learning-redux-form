8309f3927c77f4a4558f781eb6d6f42432830a0d
Man Wai Wong <william.manwai@gmail.com>
2017-10-17 23:30:13 +0800

    nested route

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,17 @@ import {Route, IndexRoute} from 'react-router';
 
 import App from './components/app';
 
+
+const Greeting =() =>{
+  return (<h1>死全家蔡育基佬</h1>);
+}
+
+
 export default(
-  <Route path="/" component={App} />
+  <Route path="/" component={App} >
+    <Route path="greet" component={Greeting} />
+    <Route path="greet2" component={Greeting} />
+    <Route path="greet4" component={Greeting} />
+  </Route>
+
 )
