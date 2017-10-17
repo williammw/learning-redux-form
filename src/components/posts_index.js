4f3c25c88648be14b55f5ec202e13485c4843d93
Man Wai Wong <william.manwai@gmail.com>
2017-10-17 23:36:05 +0800

    ndex change

---
+++ src/components/posts_index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default () => {
+  return (
+    <div>list of blogPost</div>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,16 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 
 import App from './components/app';
-
+import postIndex from './components/posts_index';
 
 const Greeting =() =>{
-  return (<h1>死全家蔡育基佬</h1>);
+  return (<h1>HI BLOG POT</h1>);
 }
 
 
 export default(
   <Route path="/" component={App} >
+    <IndexRoute component={postIndex} />
     <Route path="greet" component={Greeting} />
     <Route path="greet2" component={Greeting} />
     <Route path="greet4" component={Greeting} />
